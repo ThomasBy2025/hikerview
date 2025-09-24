c95c1c43d3215c3bcf3b423f714d085b4c1f72a6
ThomasBy2025 <1585568865@qq.com>
2025-09-24 13:02:28 +0800

    Add files via upload
    解决 
    插件在 加密播放链接 时
    无法获取用户变量的bug

--- gcsp1999/config/global.js
+++ gcsp1999/config/global.js
@@ -246,7 +246,7 @@ function getDownData(_json) {
 
 // 获取插件的用户变量
 function getUserVariables(_) {
-    let userVariables = globalMap0.getVar(rule_id + "@userVariables@" + _.platform);
+    let userVariables = globalMap0.getVar(rule_id + "@userVariables@" + _.platform, "");
     if (userVariables == "") {
         userVariables = {};
         if (Array.isArray(_.userVariables) && _.userVariables.length) {
